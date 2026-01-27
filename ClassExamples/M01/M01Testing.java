4078264c6b7c668c1baef6929cfbe9dbf8885c01
GoopSoftware <jhreschke@gmail.com>
2026-01-27 13:19:56 -0700

    Updated folder structure, added a few files

---
+++ ClassExamples/M01/M01Testing.java
@@ -0,0 +1,15 @@
+package ClassExamples.M01;
+
+import java.util.Scanner;
+
+public class M01Testing {
+
+    Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+
+
+
+    }
+}
